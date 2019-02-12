56a96aba16df3f60bf8d10714bb5df0ec44ccb31
Pedro <pedro.phdois@gmail.com>
2019-02-12 21:41:20 -0200

    login agora é feito clicando no botão

--- ng-atemsfy/src/app/modules/login/login.component.ts
+++ ng-atemsfy/src/app/modules/login/login.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../core/services/auth/auth.service';
 
 @Component({
-	selector: 'app-login',
-	templateUrl: './login.component.html',
-	styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-	constructor(
-		public authService: AuthService
-	) { }
+    constructor(
+        public authService: AuthService
+    ) { }
 
-	ngOnInit() {
-		this.authService.auth();
-	}
+    ngOnInit() { }
+
+    login() {
+        this.authService.auth();
+    }
 
 }
